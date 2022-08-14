713852c1a6579af6fad6f6aa5f21aba5b22dfae9
Eddie Sanmarco <eddie@Eddies-MacBook-Pro.local>
2022-08-14 15:17:45 -0700

    fixed fetching issues

--- src/pages/confirm-signup.tsx
+++ src/pages/confirm-signup.tsx
@@ -1,5 +1,8 @@
 import { Routes } from '@/lib/consts';
 import { DiscordAccount } from '@/models/user';
+import authenticatePage from '@/utils/authenticatePage';
+import { useQuery } from '@tanstack/react-query';
+import { Spinner } from 'flowbite-react';
 import { signIn, getSession, signOut } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
@@ -10,48 +13,53 @@ interface ConfirmSignupProps {
 
 export default function ConfirmSignup({ user }: ConfirmSignupProps) {
     const { push } = useRouter();
-    useEffect(() => {
-        if (user) {
-            getProfile();
-        }
-    }, []);
+    const { data, isLoading, status } = useQuery(['me'], async () => {
+        return await fetch('/api/users/me').then(res => res.json());
+    });
 
-    const getProfile = async () => {
-        const p = await fetch(`/api/users/me`).then(res => res.json());
-        if (p) {
+    useEffect(() => {
+        if (data?.data?.email) {
             push(Routes.Home);
         }
-    };
+    }, [data]);
 
-    return (
-        <div className="w-full h-screen flex flex-col justify-center items-center">
-            <div className="max-w-2xl mx-auto p-4 text-center">
-                <h2 className="text-xl font-bold mb-3">
-                    You do not have access to sign up for the beta yet.
-                </h2>
-                <p>Please try again later</p>
-                <button
-                    onClick={() => {
-                        if (user) {
-                            signOut();
-                        } else {
-                            signIn();
-                        }
-                    }}
-                    className="bg-black px-3 py-2 text-md text-white rounded w-fit my-3"
-                >
-                    {user ? 'Sign Out' : 'Sign In'}
-                </button>
+    if (isLoading) {
+        return (
+            <div className="flex flex-col gap-3 w-full h-screen justify-center items-center">
+                <Spinner size="xl" />
+                Checking Account...
             </div>
-        </div>
-    );
+        );
+    }
+
+    if (data?.type === 'error') {
+        return (
+            <div className="w-full h-screen flex flex-col justify-center items-center">
+                <div className="max-w-2xl mx-auto p-4 text-center">
+                    <h2 className="text-xl font-bold mb-3">
+                        You do not have access to sign up for the beta yet.
+                    </h2>
+                    <p>Please try again later</p>
+                    <button
+                        onClick={() => {
+                            if (user) {
+                                signOut();
+                            } else {
+                                signIn();
+                            }
+                        }}
+                        className="bg-black px-3 py-2 text-md text-white rounded w-fit my-3"
+                    >
+                        {user ? 'Sign Out' : 'Sign In'}
+                    </button>
+                </div>
+            </div>
+        );
+    }
+    return null;
 }
 
 export const getServerSideProps = async (context: any) => {
     const session = await getSession(context);
-    return {
-        props: {
-            user: session?.user ?? null,
-        },
-    };
+    return await authenticatePage(session);
 };
