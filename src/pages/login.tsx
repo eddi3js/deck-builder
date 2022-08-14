28dcdcc12eeb95083f3666dbfd3c705e328abf41
Eddie Sanmarco <eddie@Eddies-MacBook-Pro.local>
2022-08-13 19:14:41 -0700

    test commit

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,7 +4,7 @@ import { signIn } from 'next-auth/react';
 export default function Login() {
     return (
         <Page>
-            You are not logged in.
+            You are not logged in
             <button
                 onClick={() => signIn()}
                 className="bg-black px-3 py-2 text-md text-white rounded w-fit my-3"
