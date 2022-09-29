eba2754bed2c64669454dc11f4e20fcc3efa4a4b
Eddie Sanmarco <eddie@Eddies-MacBook-Pro.local>
2022-09-29 13:32:37 -0700

    removed session prop drilling on SessionProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         themeChange(false);
     }, []);
     return (
-        <SessionProvider session={(pageProps as any)?.session}>
+        <SessionProvider>
             <Component {...pageProps} />
         </SessionProvider>
     );
